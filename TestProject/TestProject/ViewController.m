0685a91859a51a7f05a7f00b6b8b5b4c2e6a8e12
linqinbin <874651459@qq.com>
2016-03-01 22:27:58 +0800

    //删除测试语句

--- TestProject/TestProject/ViewController.m
+++ TestProject/TestProject/ViewController.m
@@ -18,9 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello World!");
-    NSLog(@"这是提交到分支FirstBranch1");
-    NSLog(@"这是提交到分支FirstBranch");
-    NSLog(@"这是提交到分支FirstBranch2");
 }
 
 - (void)didReceiveMemoryWarning {
