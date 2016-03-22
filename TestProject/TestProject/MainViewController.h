a38c3a05914b9c7060b3c8cdb44c9d22c1d36c29
linqinbin <874651459@qq.com>
2016-03-22 23:06:25 +0800

    删除storyboard

---
+++ TestProject/TestProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TestProject
+//
+//  Created by linqinbin on 16/3/22.
+//  Copyright © 2016年 linqb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
