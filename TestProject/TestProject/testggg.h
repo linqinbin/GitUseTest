efbbe265c1bf73de4ef3d81cf7adcf0aeed79475
linqb <linqb@linqbdeMac-mini.local>
2016-04-22 15:34:57 +0800

    测试

---
+++ TestProject/TestProject/testggg.h
@@ -0,0 +1,13 @@
+//
+//  testggg.h
+//  TestProject
+//
+//  Created by linqb on 16/4/22.
+//  Copyright © 2016年 linqb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testggg : NSObject
+
+@end
---
+++ TestProject/TestProject/testggg.m
@@ -0,0 +1,13 @@
+//
+//  testggg.m
+//  TestProject
+//
+//  Created by linqb on 16/4/22.
+//  Copyright © 2016年 linqb. All rights reserved.
+//
+
+#import "testggg.h"
+
+@implementation testggg
+
+@end
