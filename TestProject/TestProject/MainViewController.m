602a0fd3bf13d623fab35e70975b06cb5f630ecc
linqinbin <874651459@qq.com>
2016-04-24 22:04:58 +0800

    测试合并撤销

--- TestProject/TestProject/MainViewController.m
+++ TestProject/TestProject/MainViewController.m
@@ -23,6 +23,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     //    [self NSInvocationTest];
+    NSLog(@"测试");
 }
 
 - (IBAction)qrBtnClick:(UIButton*)sender
