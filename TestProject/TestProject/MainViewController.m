73fd5fba9b6dc24807829f0366fe36befaae40e9
linqb <linqb@linqbdeMac-mini.local>
2016-08-24 09:06:24 +0800

    [test]

--- TestProject/TestProject/MainViewController.m
+++ TestProject/TestProject/MainViewController.m
@@ -24,7 +24,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     //    [self NSInvocationTest];
-
+    NSLog(@"test");
 }
 
 - (IBAction)qrBtnClick:(UIButton*)sender
